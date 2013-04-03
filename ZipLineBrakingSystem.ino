681b8654617c0c4301d53266839a0d5a647013cc
Erik S Carlsten <ecarlste@gmail.com>
2013-04-03 11:57:24 -0600

    Added main sketch file and defined digital pin constants.

---
+++ ZipLineBrakingSystem.ino
@@ -0,0 +1,22 @@
+// Digital Pin Mapping
+const int LED0 = 0;
+const int LED1 = 1;
+const int LED2 = 4;
+const int LED3 = 5;
+const int LED4 = 6;
+const int LED5 = 7;
+const int LED6 = 8;
+const int LED7 = 10;
+
+const int returnPin = 2;
+const int securePin = 11;
+const int servoPin = 9;
+
+
+void setup() {
+}
+
+
+void loop() {
+}
+
