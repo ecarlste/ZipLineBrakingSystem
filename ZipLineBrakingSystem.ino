c70bf9a1e92346ee6dd053eca4f2b37f5507d9a4
Erik S Carlsten <ecarlste@gmail.com>
2013-04-04 23:04:06 -0600

    Finished getSystemStatus() method. Added system status constants.

--- ZipLineBrakingSystem.ino
+++ ZipLineBrakingSystem.ino
@@ -6,38 +6,41 @@
 
 // Digital Pin Mapping
 #define SECURE_PIN 11
-#define SERVO_PIN 9
+#define SERVO_PIN  9
 
 
-// Analog Pin Mapping
-#define BATTERY_PIN 0
-
+// Battery Definitions
+#define BATTERY_PIN       0
+#define BATTERY_LEVEL_MIN 25
 
 // Temperature Sensor Definitions
-#define TMP36_VOUT 1
+#define TMP36_VOUT                    1
 #define TMP36_MILLIVOLTS_PER_DEGREE_C 10
+#define OPERATING_TEMP_MAX            70
 
 
 // Brake Servo Definitions
-#define BRAKES_OFF 0
+#define BRAKES_OFF  0
 #define BRAKES_HOLD 165
 #define BRAKES_FULL 175
+
 Servo brakeServo;
 
 
 // Accelerometer Definitions
 #define ADXL345SCL_PIN 5
 #define ADXL345SDA_PIN 4
+
 ADXL345 adxl;
 
 
 // LED Definitions
-#define LED0 7
-#define LED1 8
-#define LED2 12
-#define LED3 13
+#define LED0    7
+#define LED1    8
+#define LED2    12
+#define LED3    13
 #define LED_OFF 0
-#define LED_ON 1
+#define LED_ON  1
 
 
 // Interrupt Definitions
@@ -46,10 +49,24 @@ ADXL345 adxl;
 
 // System Operating Modes
 #define MODE_AWAITING_RETURN 0
-#define MODE_RETURNING 1
+#define MODE_RETURNING       1
 #define MODE_AWAITING_IMPACT 2
-#define MODE_BRAKING 3
+#define MODE_BRAKING         3
+
 int mode;
+
+
+// System Status Definitions
+#define BATTERY_OK           1
+#define ACCELEROMETER_OK     2
+#define CASE_SECURE_OK       4
+#define TEMP_OK              8
+#define SYSTEM_OK            15
+
+#define BATTERY_OK_LED       LED0
+#define ACCELEROMETER_OK_LED LED1
+#define CASE_SECURE_OK_LED   LED2
+#define TEMP_OK_LED          LED3
 
 
 void setup() {
@@ -140,6 +157,7 @@ void setModeReturning() {
 
 void returnToStart() {
   // check the system status to ensure ready to return
+  int status = getSystemStatus();
   
   // start return trip
   
@@ -166,6 +184,34 @@ void slowToStop() {
   // determine braking force needed to achieve the desired acceleration
   
   // once velocity is near 0, or acceleration is near 1g, set mode to return waiting
+}
+
+
+int getSystemStatus() {
+  int systemStatus = 0;
+  
+  // check battery status
+  if (getBatteryLevel() > BATTERY_LEVEL_MIN) {
+    systemStatus |= BATTERY_OK;
+  }
+  
+  // check that case is secured
+  if (isCaseSecure()) {
+    systemStatus |= CASE_SECURE_OK;
+  }
+  
+  // check temperature
+  int temperature = getTemperature();
+  if (temperature < OPERATING_TEMP_MAX) {
+    systemStatus |= TEMP_OK;
+  }
+  
+  // check accelerometer
+  if (adxl.status == ADXL345_OK) {
+    systemStatus |= ACCELEROMETER_OK;
+  }
+  
+  return systemStatus;
 }
 
 
